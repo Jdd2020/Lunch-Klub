34091b05e033f65cc424e69c38999f41c3854db5
Jdd2020 <jdd89@pitt.edu>
2025-02-26 22:34:50 -0500

    Added some routing

---
+++ client/src/auth/login/index.tsx
@@ -0,0 +1,19 @@
+const LoginLanding  = () => {
+    return (
+        <div style={{
+            display: "flex",
+            alignContent: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            width: "20%",
+            height: "100%",
+            border: "1px solid red"
+        }}>
+            <h3>Login</h3>
+            <input type="text" />
+            <input type="text" />
+        </div>
+    )
+}
+
+export default LoginLanding;
