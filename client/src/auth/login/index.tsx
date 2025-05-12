60cbbd43c39b7ba468dc40193f16a4bddcd78e80
Jdd2020 <jdd89@pitt.edu>
2025-05-12 00:42:14 -0400

    Added a logo to project

--- client/src/auth/login/index.tsx
+++ client/src/auth/login/index.tsx
@@ -1,11 +1,13 @@
 import Button from "../../component/Button";
 import "./index.css"
+import logo from "../../assets/lunch-klub.svg"
 
 const LoginLanding  = () => {
     return (
         <div className="login-landing" id="login-landing">
             <div className="login-landing-box">
                 <div>Welcome</div>
+                <img src={logo} height={100} width={100} alt="Lunch Klub Logo" className="login-logo"/>
                 <div className="login-text-input-container">
                     <div className="login-text-input-label">Username:</div>
                     <input type="text" className="login-text-input"/>
--- client/src/component/Button.tsx
+++ client/src/component/Button.tsx
@@ -9,22 +9,13 @@ type ButtonProps = {
     onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
-const PrimaryButtonStyle : React.CSSProperties = {
-    width: "100%",
-    height: "50px",
-    backgroundColor: "#FFF",
-    color: "white",
-    borderRadius: "5px",
-    border: "none",
-    cursor: "pointer",
-}
+
 
 const Button = (
     { id, className, style, children, onClick }: ButtonProps
 ) => {
     return (
         <button onClick={onClick} className={className} style={style} id={id}>
-
             {children}
         </button>
     )
