bd76452d8c98e1ac7bef03804561a6780dd9eb0f
Jdd2020 <jdd89@pitt.edu>
2025-06-08 00:02:49 -0400

    Added base and user classes

---
+++ api/src/main/java/com/lunchklub/api/models/Base.java
@@ -0,0 +1,32 @@
+package com.lunchklub.api.models;
+
+public class Base {
+    private String id;
+    private String createdAt;
+    private String updatedAt;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
