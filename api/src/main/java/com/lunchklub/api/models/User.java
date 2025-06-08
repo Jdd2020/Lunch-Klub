8959b3a90157e969efea35aabd899818208d8fc8
Jdd2020 <jdd89@pitt.edu>
2025-06-08 01:54:58 -0400

    Added a nomination class

--- api/src/main/java/com/lunchklub/api/models/User.java
+++ api/src/main/java/com/lunchklub/api/models/User.java
@@ -1,15 +1,13 @@
 package com.lunchklub.api.models;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import jakarta.persistence.Entity;
 
+@Entity
 public class User extends Base {
     private String username;
     private String email;
     private String password;
     private boolean isAdmin;
-    private String[] rooms;
 
     // Getters and Setters
     public String getUsername() {
@@ -42,34 +40,6 @@ public class User extends Base {
 
     public void setAdmin(boolean isAdmin) {
         this.isAdmin = isAdmin;
-    }
-
-    public String[] getRooms() {
-        return rooms;
-    }
-
-    public void setRooms(String[] rooms) {
-        this.rooms = rooms;
-    }
-
-    public void addRoom(String roomId, Room room) {
-        if (this.rooms == null) {
-            this.rooms = new String[1];
-            this.rooms[0] = roomId;
-        } else {
-            String[] newRooms = new String[this.rooms.length + 1];
-            System.arraycopy(this.rooms, 0, newRooms, 0, this.rooms.length);
-            newRooms[this.rooms.length] = roomId;
-            this.rooms = newRooms;
-        }
-    }
-
-    public void removeRoom(String roomId) {
-        if (this.rooms != null) {
-            List<String> roomList = new ArrayList<>(Arrays.asList(this.rooms));
-            roomList.remove(roomId);
-            this.rooms = roomList.toArray(new String[0]);
-        }
     }
 
 }
