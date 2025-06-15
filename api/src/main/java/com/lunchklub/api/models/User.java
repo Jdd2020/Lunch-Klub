dcc0b596ce7f464939e3a8f535205d62a6b5fdfc
Jdd2020 <jdd89@pitt.edu>
2025-06-15 14:22:14 -0400

    Annontated login class

--- api/src/main/java/com/lunchklub/api/models/User.java
+++ api/src/main/java/com/lunchklub/api/models/User.java
@@ -1,14 +1,26 @@
 package com.lunchklub.api.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User extends Base {
+
+    @Column(name = "username", nullable = false, unique = true, length = 50)
     private String username;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
     private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
+
+    @Column(name = "last_login")
+    private String lastLogin;
+
+    @Column(name = "is_admin", nullable = false)
     private boolean isAdmin;
 
     // Getters and Setters
@@ -34,6 +46,14 @@ public class User extends Base {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(String lastLogin) {
+        this.lastLogin = lastLogin;
     }
 
     public boolean isAdmin() {
