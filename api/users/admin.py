9bed5087608d9673ca93c591d1459cd7041c870d
Jdd2020 <jdd89@pitt.edu>
2025-07-20 20:22:21 -0400

    Added user admin page

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'is_staff', 'is_active')}
+        ),
+    )
+
+admin.site.register(User, UserAdmin)
