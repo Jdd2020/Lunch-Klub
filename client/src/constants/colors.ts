3d15d19bbacd92cc579a0c7702a0052becd15014
Jdd2020 <jdd89@pitt.edu>
2025-09-01 12:43:54 -0400

    Removed commented color constant

--- client/src/constants/colors.ts
+++ client/src/constants/colors.ts
@@ -1,12 +1,3 @@
-// :root {
-//   --background-gradient: linear-gradient(270deg, #d72c2c, #e2832b, #d72c2c);
-//   --primary-light-gray: #ababab;
-//   --secondary-light-gray: #8b8383;
-//   --primary-gray: #595757;
-//   --secondary-gray: #615d5d;
-//   --error-red: #cf4747;
-// }
-
 const colorPallette = {
     primaryGray: "var(--primary-gray)",
     secondaryGray: "var(--secondary-gray)",
