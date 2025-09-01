cd3858c32a02d06c66754f52af4c0ac87ea59799
Jdd2020 <jdd89@pitt.edu>
2025-08-31 23:00:24 -0400

    All colors converted

--- client/src/constants/colors.ts
+++ client/src/constants/colors.ts
@@ -1,10 +1,19 @@
+// :root {
+//   --background-gradient: linear-gradient(270deg, #d72c2c, #e2832b, #d72c2c);
+//   --primary-light-gray: #ababab;
+//   --secondary-light-gray: #8b8383;
+//   --primary-gray: #595757;
+//   --secondary-gray: #615d5d;
+//   --error-red: #cf4747;
+// }
+
 const colorPallette = {
-    backgroundGray: "#595757",
-    textLightGray: "#d3d3d3",
-    textGray: "#ababab",
-    borderGray: "#615d5d",
-    borderHoverGray: "#8b8383",
-    white: "#ffffff",
+    primaryGray: "var(--primary-gray)",
+    secondaryGray: "var(--secondary-gray)",
+    errorRed: "var(--error-red)",
+    backgroundGradient: "var(--background-gradient)",
+    primaryLightGray: "var(--primary-light-gray)",
+    secondaryLightGray: "var(--secondary-light-gray)",
 };
 
 export default colorPallette;
